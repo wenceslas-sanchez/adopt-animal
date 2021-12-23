8e459558de50846613eaaebbe2db34e99cc2fff8
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-23 16:59:49 +0100

    ✨ rebuild project goal : adopt an animal with characteristic (name, age..)

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,23 +1,52 @@
 pragma solidity ^0.8.11;
 
-contract Adoption {
-    address[] public adopters;
-    uint petNumber= 0;
+import {AdoptionUtils} from "./AdoptionUtils.sol";
 
-    function adopt(uint _petId) public returns (uint) {
-        require(_petId > 0 && _petId <= petNumber + 2, "The pet you want to adopt doesn't exist yet.");
-        adopters[_petId]= msg.sender;
-        petNumber++;
-        return _petId;
+contract Adoption {
+    struct Animal {
+        string name;
+        string kind;
+        uint age;
+    }
+    uint constant maxAnimal= 10; // max number of available animals.
+    uint constant dogProba= 60;
+    uint8 randNone= 0; // may overflow, this is not an issue
+    address[maxAnimal] adopters;
+    Animal public animals;
+
+    event AdoptAnimalEvent(uint id, address owner);
+
+    function adoptAnimal(uint _petId) public {
+        require(_petId>= 0 && _petId < maxAnimal);
+        if (adopters[_petId] == address(0)) {
+            adopters[_petId] = msg.sender;
+            emit AdoptAnimalEvent(_petId, msg.sender);
+        } else {
+            revert("This animal already has a home.");
+        }
     }
 
-    /*
-     * @dev Public vision of data location adopters.
-     */
-    function getAdopters() public view returns (address[] memory) {
+    function generateAnimalKind() private returns (string memory){
+        uint proba= uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))) % 100;
+        randNone++; // can overflow, this is not an issue
+        if (proba < dogProba) {
+            return "dog";
+        } else {
+            return "cat";
+        }
+    }
+
+    function generateAnimalName() private view returns (string memory) {
+        return AdoptionUtils.uintToString(uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))));
+    }
+
+    function generateAnimalAge() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))) % 100;
+    }
+
+    function getAdopters() public view returns (address[maxAnimal] memory) {
         return adopters;
     }
 
-
-
 }
+
