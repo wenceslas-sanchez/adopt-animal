a680b259b3249e0144529807b150d3c30ebb15b3
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-23 18:46:49 +0100

    ⚡️generate animals with kind and age

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,24 +1,25 @@
 pragma solidity ^0.8.11;
 
-import {AdoptionUtils} from "./AdoptionUtils.sol";
-
 contract Adoption {
     struct Animal {
-        string name;
         string kind;
         uint age;
     }
     uint constant maxAnimal= 10; // max number of available animals.
     uint constant dogProba= 60;
     uint8 randNone= 0; // may overflow, this is not an issue
-    address[maxAnimal] adopters;
-    Animal public animals;
+    address[maxAnimal] private adopters;
+    Animal[maxAnimal] private animals;
 
     event AdoptAnimalEvent(uint id, address owner);
 
     function adoptAnimal(uint _petId) public {
         require(_petId>= 0 && _petId < maxAnimal);
         if (adopters[_petId] == address(0)) {
+            animals[_petId]= Animal(
+                generateAnimalKind(),
+                generateAnimalAge()
+            );
             adopters[_petId] = msg.sender;
             emit AdoptAnimalEvent(_petId, msg.sender);
         } else {
@@ -26,26 +27,27 @@ contract Adoption {
         }
     }
 
-    function generateAnimalKind() private returns (string memory){
+    function generateAnimalKind() public returns (string memory kind){
         uint proba= uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))) % 100;
         randNone++; // can overflow, this is not an issue
         if (proba < dogProba) {
-            return "dog";
+            kind= "dog";
         } else {
-            return "cat";
+            kind= "cat";
         }
+        return kind;
     }
 
-    function generateAnimalName() private view returns (string memory) {
-        return AdoptionUtils.uintToString(uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))));
-    }
-
-    function generateAnimalAge() private view returns (uint) {
+    function generateAnimalAge() public view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNone))) % 100;
     }
 
     function getAdopters() public view returns (address[maxAnimal] memory) {
         return adopters;
+    }
+
+    function getAnimals() public view returns (Animal[maxAnimal] memory) {
+        return animals;
     }
 
 }
