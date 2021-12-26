81b065325a13741535d341f19b28f08ad5d5d251
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-26 17:05:38 +0100

    ✅ update test for method available pet id

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -7,7 +7,7 @@ import {Adoption} from "../contracts/Adoption.sol";
 contract TestAdoption {
     // defined in Adoption contract
     uint constant MAX_ANIMAL= 10; // max number of available animals.
-    // Instantiate contract
+    // Instantiate global contract
     Adoption adoption= Adoption(DeployedAddresses.Adoption());
 
     function testAdoptAnimal() public {
@@ -22,7 +22,7 @@ contract TestAdoption {
             );
         }
         // After adoption, there should be one adopter address.
-        adoption.adoptAnimal(0);
+        adoption.adoptAnimal(0, "Alice");
         resultAdopters= adoption.getAdopters();
         expectedAdopters[0]= address(this);
         Assert.equal(address(this), resultAdopters[0],
@@ -33,15 +33,29 @@ contract TestAdoption {
     // For testAdoptAnimalAlreadyAdopted method only
     function raiseAdoptAnimalAlreadyAdopted() public {
         // raise an error if called after adoption.adoptAnimal(1).
-        adoption.adoptAnimal(1);
+        adoption.adoptAnimal(1, "Bob");
     }
-
     function testAdoptAnimalAlreadyAdopted() public {
         bool r;
         // Once called should work
-        adoption.adoptAnimal(1);
+        adoption.adoptAnimal(1, "Bob");
         // Again for same _petId=1 should not work
         (r, ) = address(this).call(abi.encodePacked(this.raiseAdoptAnimalAlreadyAdopted.selector));
         Assert.isFalse(r, "Should not adopt same _petId twice.");
+    }
+
+    function testGetAvailablePetId() public {
+        uint[] memory expectedAvailablePetId;
+        uint[] memory resultAvailablePetId;
+        adoption.adoptAnimal(2, "Alice");
+        adoption.adoptAnimal(3, "Alice");
+        adoption.adoptAnimal(4, "Alice");
+        adoption.adoptAnimal(5, "Alice");
+        adoption.adoptAnimal(6, "Alice");
+        adoption.adoptAnimal(7, "Alice");
+        adoption.adoptAnimal(8, "Alice");
+        resultAvailablePetId= adoption.getAvailablePetId();
+        Assert.equal(resultAvailablePetId.length, 1, "Should stay only one pet.");
+        Assert.equal(resultAvailablePetId[0], 9, "Should stay the 10-th pet.");
     }
 }
