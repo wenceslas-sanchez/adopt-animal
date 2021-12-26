c5a400223a99d6468c223451a98788c4077f9fa9
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-26 18:42:47 +0100

    🚧 set ERC721 architecture

---
+++ contracts/AnimalOwnership.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Adoption} from "./Adoption.sol";
+
+contract AnimalOwnership is Adoption, Ownable, ERC721 {
+    constructor() ERC721("TestAnimalAdoption", "ANI"){
+    }
+
+    function balanceOf(address _owner) public override view returns (uint256 _balance) {
+        return 1;
+    }
+
+    function ownerOf(uint256 _tokenId) public override view returns (address _owner) {
+        return address(0);
+    }
+
+    function transferFrom(address _from, address _to, uint _tokenId) public override {
+
+    }
+
+    function approve(address _to, uint256 _tokenId) public override {
+
+    }
+
+    function getApproved(uint256 _tokenId) public view override returns (address _operator) {
+        return address(0);
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) public override {
+
+    }
+
+    function isApprovedForAll(address _owner, address _operator) public view override returns (bool) {
+        return false;
+    }
+
+    function safeTransferFrom(address _from, address _to, uint _tokenId) public override {
+
+    }
+
+    function safeTransferFrom(address _from, address _to, uint _tokenId, bytes memory _data) public override {
+
+    }
+
+}
