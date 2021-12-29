22f4efdf8c255ca91681586ae8773d510214378e
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-29 11:25:15 +0100

    ✅ update test with state variable

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -13,19 +13,16 @@ contract TestAdoption {
     function testAdoptAnimal() public {
         // Before adoption, adopters is full fo null address
         address[] memory expectedAdopters= new address[](MAX_ANIMAL);
-        address[MAX_ANIMAL] memory resultAdopters;
 
-        resultAdopters= adoption.getAdopters();
         for (uint i= 0; i < MAX_ANIMAL; i++) {
-            Assert.equal(address(0), resultAdopters[i],
+            Assert.equal(address(0), adoption.adopters(i),
                 "Should only contain null addresses."
             );
         }
         // After adoption, there should be one adopter address.
         adoption.adoptAnimal(0, "Alice");
-        resultAdopters= adoption.getAdopters();
         expectedAdopters[0]= address(this);
-        Assert.equal(address(this), resultAdopters[0],
+        Assert.equal(address(this), adoption.adopters(0),
             "First pet should have an owner."
         );
     }
