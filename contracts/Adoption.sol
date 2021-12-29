0e84ba48a8c7ed9ffcc2548a0688da905cf8c089
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-29 11:08:36 +0100

    🚨 set animals and owner public

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -11,8 +11,8 @@ contract Adoption {
     uint constant MAX_ANIMAL= 10; // max number of available animals.
     uint constant DOG_PROBA= 60;
     uint8 randNone= 0; // may overflow, this is not an issue
-    address[MAX_ANIMAL] private adopters;
-    Animal[MAX_ANIMAL] private animals;
+    address[MAX_ANIMAL] public adopters;
+    Animal[MAX_ANIMAL] public animals;
 
     event AdoptAnimalEvent(uint id, address owner);
 
