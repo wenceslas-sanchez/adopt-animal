b72b5556b5e22a6fccd4522fde2faf5115a6f118
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-30 17:15:42 +0100

    🔧 set ropsten config network

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 
 module.exports = {
     networks: {
@@ -16,6 +19,14 @@ module.exports = {
             port: 7545,
             network_id: "*" // match any network
         },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(process.env.MNEMONIC,
+                    `https://:${process.env.PROJECT_SECRET}@ropsten.infura.io/v3/${process.env.PROJECT_ID}`, 1)
+            },
+            network_id: 3,
+            from: "0x00A010F72B6385b374b691f9d012286454C82b77"
+        }
     },
     compilers: {
         solc: {
