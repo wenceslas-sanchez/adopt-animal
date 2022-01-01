b13f788cf5995e46b64eddc6c882c969144b602f
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-01-01 21:07:37 +0100

    🚧 generalise etherscan GET request

--- src/etherscan/etherscan.py
+++ src/etherscan/etherscan.py
@@ -25,23 +25,41 @@ def generate_etherscan_url(network: NetworkEnum) -> str:
     return f"https://api{network_value}.etherscan.io/api"
 
 
-def get_contract_abi(network: NetworkEnum, address: str):
-    base_url= generate_etherscan_url(network)
+def get_etherscan_response(network: NetworkEnum, params: dict):
+    base_url = generate_etherscan_url(network)
     response = requests.get(
         base_url,
-        params={
-            "module": "contract",
-            "action": "getabi",
-            "address": address,
-            "tag": "latest",
-            "apikey": API_KEY,
-        },
-        headers= requests.utils.default_headers()
+        params= params,
+        headers=requests.utils.default_headers()
     )
 
     # TODO : set HTTP error, not Exception (mais j'ai la flemme pour le moment)
     if response.status_code != 200:
         raise Exception(response.text)
+
+    return json.loads(response.text)
+
+
+def get_contract_abi(network: NetworkEnum, address: str):
+    params= {
+            "module": "contract",
+            "action": "getabi",
+            "address": address,
+            "tag": "latest",
+            "apikey": API_KEY,
+        }
+    return get_etherscan_response(network, params)
+
+
+def get_last_block(network: NetworkEnum):
+    params= {
+            "module": "block",
+            "action": "getblocknobytime",
+            "timestamp": round(time.time()) + 1,
+            "closest": "before",
+            "apikey": API_KEY,
+        }
+    return get_etherscan_response(network, params)
 
 
 get_contract_abi(NetworkEnum.mainnet, "0xB3de37059C441eBC4B6DFFB903Af5FB5b94e2dCe")
