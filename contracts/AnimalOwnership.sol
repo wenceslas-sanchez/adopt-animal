ea1db74e14f63c4fd383ce59fa7a7700542f7808
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-01-02 17:52:59 +0100

    🚧 start implementing ERC721 methods

--- contracts/AnimalOwnership.sol
+++ contracts/AnimalOwnership.sol
@@ -8,13 +8,29 @@ contract AnimalOwnership is Adoption, Ownable, ERC721 {
     constructor() ERC721("TestAnimalAdoption", "ANI"){
     }
 
+    mapping (uint => address) animalApprovals;
+    int8[MAX_ANIMAL] petIdApprovals; // store animalApprovals keys
+
     modifier onlyOwnerOf(uint _petId) {
         require(msg.sender == ownerOf(_petId));
         _;
     }
 
-    function balanceOf(address _owner) public override view returns (uint256 _balance) {
-        return 1;
+    // From https://ethereum.stackexchange.com/questions/1527/how-to-delete-an-element-at-a-certain-index-in-an-array/1528
+    function _burn(uint index) internal {
+        require(index < array.length);
+        array[index] = array[array.length-1];
+        array.pop();
+    }
+
+    function balanceOf(address _owner) public override view returns (uint) {
+        uint _balance= 0;
+        for (uint i= 0; i < MAX_ANIMAL; i++) {
+            if (adopters[i] == _owner) {
+                _balance++;
+            }
+        }
+        return _balance;
     }
 
     function ownerOf(uint256 _tokenId) public override view petIdExists(_tokenId) returns (address) {
@@ -22,15 +38,22 @@ contract AnimalOwnership is Adoption, Ownable, ERC721 {
     }
 
     function transferFrom(address _from, address _to, uint _tokenId) public override {
-
     }
 
-    function approve(address _to, uint256 _tokenId) public override {
-
+    function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) override {
+        animalApprovals[_tokenId]= _to;
+        petIdApprovals.push(uint8(_tokenId));
+        Approval(msg.sender, _to, _tokenId);
     }
 
     function getApproved(uint256 _tokenId) public view override returns (address _operator) {
-        return address(0);
+        _operator= address(0);
+        for (uint8 i= 0; i< petIdApprovals.length + 1; i++) {
+            if (petIdApprovals[i] == uint8(_tokenId)) {
+                _operator= animalApprovals[_tokenId];
+                break;
+            }
+        }
     }
 
     function setApprovalForAll(address _operator, bool _approved) public override {
@@ -38,11 +61,8 @@ contract AnimalOwnership is Adoption, Ownable, ERC721 {
     }
 
     function isApprovedForAll(address _owner, address _operator) public view override returns (bool) {
+
         return false;
-    }
-
-    function safeTransferFrom(address _from, address _to, uint _tokenId) public override {
-
     }
 
     function safeTransferFrom(address _from, address _to, uint _tokenId, bytes memory _data) public override {
