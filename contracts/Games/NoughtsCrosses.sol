4b30e6824f3a0b13b99ef22d0ba2147aa22ae901
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-01-09 18:35:44 +0100

    ⚡️define once variable while checking victory

--- contracts/Games/NoughtsCrosses.sol
+++ contracts/Games/NoughtsCrosses.sol
@@ -9,9 +9,9 @@ contract NoughtsCrosses is Game {
     uint8[2] playerScoreWin = [uint8(3), uint8(12)];
 
     function generateGameFrame()
-        public
-        pure
-        returns (uint8[frameSize][frameSize] memory)
+    public
+    pure
+    returns (uint8[frameSize][frameSize] memory)
     {
         uint8[frameSize][frameSize] memory frame;
 
@@ -35,21 +35,24 @@ contract NoughtsCrosses is Game {
         } else {
             y = uint8(x);
         }
+        return y;
     }
 
     // TODO: performance optimisation
-    function check(uint8[frameSize][frameSize] memory _frame, uint256 _player)
-        public
-        view
-        returns (bool)
+    function checkWinner(uint8[frameSize][frameSize] memory _frame, uint256 _player)
+    public
+    view
+    returns (bool)
     {
-        bool _iswin = false;
+        uint8 h;
+        uint8 v;
+        uint8 rd_i;
+        uint8 ld= 0;
+        uint8 rd= 0;
         for (uint8 i = 0; i < frameSize; i++) {
-            uint8 h = 0;
-            uint8 v = 0;
-            uint8 ld = 0;
-            uint8 rd = 0;
-            uint8 rd_i = _abs8(int8(i) - int8(frameSize) - 1);
+            h = 0;
+            v = 0;
+            rd_i = _abs8(int8(i) - int8(frameSize) + 1);
             for (uint8 j = 0; j < frameSize; j++) {
                 // check horizontal
                 h += _frame[i][j];
@@ -57,14 +60,18 @@ contract NoughtsCrosses is Game {
                 v += _frame[j][i];
             }
             if (h == playerScoreWin[_player] || v == playerScoreWin[_player]) {
-                _iswin = true;
+                return true;
             }
             // check left diagonal
             ld += _frame[i][i];
             // check right diagonal
             rd += _frame[i][rd_i];
+
+            if (ld == playerScoreWin[_player] || rd == playerScoreWin[_player]) {
+                return true;
+            }
         }
 
-        return _iswin;
+        return false;
     }
 }
